41b3b5d4e975284515f331344bb29a8dedc7b68b
Jackie Cochran <cochrajg@bc.edu>
2020-10-27 16:23:33 -0400

    Initial Commit

---
+++ Snacktacular/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snacktacular
+//
+//  Created by Jackie Cochran on 10/27/20.
+//  Copyright © 2020 Jackie Cochran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
